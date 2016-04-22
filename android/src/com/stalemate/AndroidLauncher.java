1fd0958d63690a94ee231ddd1bdca5dc5aa994cb
kuosa <su.a.kuo@gmail.com>
2016-04-21 22:38:55 -0700

    Initiated LibGDX project using Android Studio

---
+++ android/src/com/stalemate/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.stalemate;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.stalemate.Stalemate;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Stalemate(), config);
+	}
+}
