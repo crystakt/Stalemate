9d07fda053af2e7185c24749bdab94c4af6d270a
kuosa <su.a.kuo@gmail.com>
2016-04-23 14:55:08 -0700

    Adding foundation for 3D element

--- core/src/com/stalemate/GameScreen.java
+++ core/src/com/stalemate/GameScreen.java
@@ -2,7 +2,16 @@ package com.stalemate;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 /**
  * Created by suchi on 4/21/16.
@@ -11,16 +20,49 @@ public class GameScreen implements Screen
 {
     final Stalemate game;
 
+    public PerspectiveCamera camera;
+    public ModelBatch modelBatch;
+    public ModelBuilder modelBuilder;
+    public Model model;
+    public ModelInstance modelInstance;
+
     public GameScreen(final Stalemate gam)
     {
         this.game = gam;
+
+        camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(0f, 0f, 3f);
+        camera.lookAt(0f, 0f, 0f);
+        camera.near = 1f;
+        camera.far  = 1f;
+        camera.update();
+
+        modelBatch = new ModelBatch();
+        modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(2f, 2f, 2f,
+                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+                VertexAttributes.Usage.Position  | VertexAttributes.Usage.Normal);
+        modelInstance = new ModelInstance(model);
     }
 
     @Override
     public void render(float delta)
     {
         Gdx.gl.glClearColor(255, 255, 255, 0);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        modelBatch.begin(camera);
+        modelBatch.render(modelInstance);
+        modelBatch.end();
+    }
+
+
+    @Override
+    public void dispose()
+    {
+        modelBatch.dispose();
+        model.dispose();
     }
 
     @Override
@@ -53,9 +95,4 @@ public class GameScreen implements Screen
 
     }
 
-    @Override
-    public void dispose()
-    {
-
-    }
 }
--- core/src/com/stalemate/MainMenuScreen.java
+++ core/src/com/stalemate/MainMenuScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 //import com.badlogic.gdx.scenes.scene2d.Stage;
 //import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
@@ -12,7 +13,7 @@ public class MainMenuScreen implements Screen
 {
     final Stalemate game;
 
-    OrthographicCamera camera;
+    public OrthographicCamera camera;
 
 //    Stage stage;
 //    TextButton playButton;
